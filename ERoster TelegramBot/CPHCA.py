ff0f9c1b442559b1f0c34842360b12712665c294
EangJS <88082498+EangJS@users.noreply.github.com>
2022-04-23 00:36:27 +0800

    Update CPHCA.py

--- ERoster TelegramBot/CPHCA.py
+++ ERoster TelegramBot/CPHCA.py
@@ -57,25 +57,7 @@ for i in range(0,50):
     role = 'HCA'
     cp(i,driver,role)
 
-
-
-
-
 driver.quit()
 print("Completed Successfully")
 
 
-
-
-    
-
-
-
-
-
-#sign in
-#driver.find_element_by_xpath('/html/body/div/div/div[1]/div/div/div[2]/form/div[1]/table/tbody/tr["%s"]/td[2]/p[5]/button[1]' % i).click()
-#driver.switch_to.alert.dismiss()
-#sign in by monthly increment
-#string = f'[id$=btnCI_6453{k}]'
-#driver.find_element_by_css_selector(string).click()
