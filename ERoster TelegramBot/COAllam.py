e7b83c57e580b21b0752c95359dd90bca02a4658
EangJS <88082498+EangJS@users.noreply.github.com>
2022-04-23 00:35:03 +0800

    Update COAllam.py

--- ERoster TelegramBot/COAllam.py
+++ ERoster TelegramBot/COAllam.py
@@ -42,18 +42,3 @@ for i in range(1,50):
 driver.quit()
 print("Completed Successfully")
 
-
-
-
-    
-
-
-
-
-
-#sign in
-#driver.find_element_by_xpath('/html/body/div/div/div[1]/div/div/div[2]/form/div[1]/table/tbody/tr["%s"]/td[2]/p[5]/button[1]' % i).click()
-#driver.switch_to.alert.dismiss()
-#sign in by monthly increment
-#string = f'[id$=btnCI_6453{k}]'
-#driver.find_element_by_css_selector(string).click()
